68a649ba2da7edf98b74632bb1db73ecfc1a28b4
Davudbarnyak23 <barnakdavid49@gmail.com>
2024-06-29 14:11:11 +0300

    mi2comit

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/name')
+def index(name=""):
+    return render_template('index.html', name=name)
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
 
 # toko dla server
 if __name__ == '__main__':
